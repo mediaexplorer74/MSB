bf4ac9206d445edbf19beeb4f0b27c1cf7ac2715
Andrey Zakharov <mediaexplorer74@hotmail.com>
2025-01-10 20:49:52 +0300

    .

--- Src/MSB/MSB/Secrets.cs
+++ Src/MSB/MSB/Secrets.cs
@@ -2,8 +2,8 @@
 {
     internal class Secrets 
     {
-        internal static string ClientID = "2396CQ";
-        internal static string ClientConsumerSecret = "e21e9ab3322a0806eed90b3c4a691551";
+        internal static string ClientID = "";
+        internal static string ClientConsumerSecret = "";
 
         internal static string CallbackURI =
           //"https://www.fitbit.com/oauth/callback";
